c005687db5ca57c5b66b379a89ec054c7c8459f4
oscar82722 <ta604232020@gmail.com>
2022-08-24 16:32:56 +0800

    fix train py

--- src/estimator/classifier.py
+++ src/estimator/classifier.py
@@ -6,16 +6,19 @@ from sklearn.ensemble import GradientBoostingClassifier
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+# install scikit-optimize
+from skopt.space import Real, Categorical, Integer
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import GridSearchCV
 from sklearn.datasets import make_classification
 from sklearn.metrics import cohen_kappa_score
-from sklearn.metrics import roc_auc_score
 from imblearn.over_sampling import SMOTE
 from sklearn.metrics import recall_score
 from sklearn.metrics import make_scorer
 from imblearn.pipeline import Pipeline
+from sklearn.metrics import get_scorer
 from xgboost import XGBClassifier
+from skopt import BayesSearchCV
 from datetime import datetime
 from sklearn import tree
 import lightgbm as lgb
@@ -39,126 +42,172 @@ def grid_info(grid, m):
 
 
 def customize_classifier(X, y, model_name, model_params='',
-                         imbalance=False, val_score=False):
+                         search_method='grid',
+                         score='AUC',
+                         imbalance=False,
+                         imbalance_process={'over': {'sampling_strategy': 0.5},
+                                            'under': {'sampling_strategy': 1}},
+                         val_score=False):
     """
     classifier estimator
 
     :param X: DataFrame/array, x variable data
     :param y: DataFrame/1.d array, y variable data only 0,1
     :param model_name: list, need train model method name
+    :param search_method: string, search_method name grid/bayes
+    :param score: string, score name
     :param model_params: dict, customize model params
     :param imbalance: boolean, imbalance adjust
+    :param imbalance_process: dict, imbalance process
     :param val_score: boolean, return val score
     :return: best_est: object, best model,
              res:DataFrame, grid search detail info
              val info :DataFrame, each val score
     """
 
-    # model method
-    model_dict = {
-
-        "tree": {"model": tree.DecisionTreeClassifier(),
-                 "params": {
-                     "step1": {'model__max_depth': [3, 5]}}
-                 },
-
-        "rf": {"model": RandomForestClassifier(),
-               "params": {
-                   "step1": {'model__max_depth': [3, 5]}}
-               },
-
-        "gbm": {"model": GradientBoostingClassifier(
-            random_state=0),
-                "params": {"step1": {
-                    'model__n_estimators': [10, 30, 50]},
-                           "step2": {
-                               'model__max_depth': [3, 5]}}
-                },
-
-        "xgb": {"model": XGBClassifier(
+    # method dict
+    method_dict = {
+        "tree": tree.DecisionTreeClassifier(),
+        "rf": RandomForestClassifier(),
+        "gbm": GradientBoostingClassifier(random_state=0),
+        "xgb": XGBClassifier(
             colsample_bytree=0.8,
             objective='binary:logistic',
             scale_pos_weight=1,
             eval_metric='error',
             use_label_encoder=False),
-            "params": {
-                    'step1': {'model__max_depth': [3, 5],
-                              'model__min_child_weight': [
-                                  1e-1, 1, 1e1]},
-                    'step2': {'model__gamma': [0, 0.2]},
-                    'step3': {
-                        'model__subsample': [0.8, 0.9]},
-                    'step4': {
-                        'model__reg_alpha': [0, 1e-1, 1,
-                                             2]},
-                    'step5': {
-                        'model__learning_rate': [0.1, 1]}}
-                },
+        "lgb": lgb.LGBMClassifier(),
+        "lg": LogisticRegression(solver='saga')
+    }
 
-        "lgb": {"model": lgb.LGBMClassifier(),
-                "params": {
-                    'step1': {
-                        'model__num_leaves': [5, 10, 20]},
-                    'step2': {
-                        'model__min_child_weight': [
-                            1e-1, 1, 1e1]},
-                    'step3': {
-                        'model__subsample': [
-                            0.5, 0.8, 0.9, 1],
-                        'model__colsample_bytree': [
-                            0.5, 0.8, 0.9, 1]},
-                    'step4': {
-                        'model__reg_alpha': [0, 1e-1, 1]}}
-                },
-        'lg': {"model": LogisticRegression(solver='saga'),
-               "params": {
-                   'step1': {'model__penalty': ['l2']}}
-               }
+    # grid Search dict
+    grid_dict = {
+        "tree": {
+            "step1": {"model__max_depth": [3, 5]}
+        },
+        "rf": {
+            "step1": {"model__max_depth": [3, 5]}
+        },
+        "gbm": {
+            "step1": {"model__n_estimators": [10, 30, 50]},
+            "step2": {"model__max_depth": [3, 5]}
+        },
+        "xgb": {
+            "step1": {"model__max_depth": [3, 5],
+                      "model__min_child_weight": [1e-1, 1, 1e1]},
+            "step2": {"model__gamma": [0, 0.2]},
+            "step3": {"model__subsample": [0.8, 0.9]},
+            "step4": {"model__reg_alpha": [0, 1e-1, 1, 2]},
+            "step5": {"model__learning_rate": [0.1, 1]}
+        },
+        "lgb": {
+            "step1": {"model__num_leaves": [5, 10, 20]},
+            "step2": {"model__min_child_weight": [1e-1, 1, 1e1]},
+            "step3": {"model__subsample": [0.5, 0.8, 0.9, 1],
+                      "model__colsample_bytree": [0.5, 0.8, 0.9, 1]},
+            "step4": {"model__reg_alpha": [0, 1e-1, 1]}
+        },
+        "lg": {
+            "step1": {"model__penalty": ["l2"]}
+        }
+    }
+
+    # Bayes Search dict
+    bayes_dict = {
+        "tree": {
+            "step1": {"model__max_depth":  Integer(1, 15),
+                      "model__min_samples_split": Integer(2, 15),
+                      "model__min_samples_leaf": Integer(1, 10)}
+        },
+        "rf": {
+            "step1": {"model__max_depth": Integer(1, 15),
+                      "model__n_estimators": Integer(100, 500)}
+        },
+        "gbm": {
+            "step1": {"model__n_estimators": Integer(10, 60),
+                      "model__max_depth": Integer(1, 15)}
+        },
+        "xgb": {
+            "step1": {
+                "model__max_depth": Integer(1, 15),
+                "model__min_child_weight": Real(0.001, 10, prior='uniform'),
+                "model__gamma": Real(0, 5, prior='uniform'),
+                "model__subsample": Real(0.6, 1, prior='uniform'),
+                "model__reg_alpha": Real(0, 3, prior='uniform'),
+                "model__learning_rate": Real(0.001, 100, prior='uniform')}
+        },
+        "lgb": {
+            "step1": {
+                "model__num_leaves": Integer(5, 30),
+                "model__min_child_weight": Real(0.001, 10, prior='uniform'),
+                "model__subsample": Real(0.6, 1, prior='uniform'),
+                "model__colsample_bytree": Real(0.6, 1, prior='uniform'),
+                "model__reg_alpha": Real(0, 3, prior='uniform')}
+        },
+        "lg": {
+            "step1": {
+                "model__penalty": Categorical(["l2"]),
+                "model__C": Real(1, 10, prior='uniform')}
+        }
+    }
+
+    # imbalance
+    imbalance_dict = {
+        "over": SMOTE(sampling_strategy=0.1),
+        "under":  RandomUnderSampler(sampling_strategy=1)
     }
 
     # scoring dict
-    scoring = {'Accuracy': 'accuracy',
-               'sensitivity': 'recall',
-               'specificity': make_scorer(recall_score,
-                                          pos_label=0),
-               'Balanced_Accuracy': 'balanced_accuracy',
-               'AUC': 'roc_auc',
-               'Kappa': make_scorer(cohen_kappa_score),
-               'F1': 'f1'}
+    scoring_dict = {
+        'Accuracy': get_scorer('accuracy'),
+        'sensitivity': get_scorer('recall'),
+        'specificity': make_scorer(recall_score, pos_label=0),
+        'Balanced_Accuracy': get_scorer('balanced_accuracy'),
+        'AUC': get_scorer('roc_auc'),
+        'Kappa': make_scorer(cohen_kappa_score),
+        'F1': get_scorer('f1')}
 
-    # set params
-    if model_params == '':
-        model_params = {your_key: model_dict[your_key][
-            'params'] for your_key in model_name}
+    # set search params
+    if model_params == '' and search_method == 'grid':
+        model_params = {your_key: grid_dict[your_key] for your_key in model_name}
+    elif search_method == 'bayes':
+        model_params = {your_key: bayes_dict[your_key] for your_key in model_name}
 
     # balance adjust
     if imbalance:
-        model_pipe = [
-            ('over', SMOTE(sampling_strategy=0.1)),
-            ('under', RandomUnderSampler(
-                sampling_strategy=0.5))]
+        model_pipe = [(i, imbalance_dict[i].set_params(**j))
+                      for i, j in imbalance_process.items()]
     else:
         model_pipe = []
 
-    # grid search
+    # run search
     md_dict, cv_result = {}, pd.DataFrame()
     for i in model_name:
         print("========== " + i + " Begin ==========")
         md = model_pipe + \
-             [('model', model_dict[i]['model'])]
+             [('model', method_dict[i])]
         pipeline = Pipeline(steps=md)
         for step, param in model_params[i].items():
             # start time
             start = datetime.now()
 
             # grid search
-            gs = GridSearchCV(estimator=pipeline,
-                              param_grid=param,
-                              scoring='roc_auc',
-                              cv=3)
-            gs.fit(X, y)
+            if search_method == 'grid':
+                search = GridSearchCV(estimator=pipeline,
+                                      param_grid=param,
+                                      scoring=scoring_dict[score],
+                                      cv=3)
+            else:
+                search = BayesSearchCV(estimator=pipeline,
+                                       search_spaces=param,
+                                       n_iter=70,
+                                       cv=3,
+                                       n_jobs=8,
+                                       scoring=scoring_dict[score],
+                                       random_state=123)
+            search.fit(X, y)
             # end time
-            res = grid_info(gs, i)
+            res = grid_info(search, i)
             t = datetime.now() - start
             res['time'] = t
             # save
@@ -166,9 +215,9 @@ def customize_classifier(X, y, model_name, model_params='',
                                    res])
 
             # update
-            pipeline.set_params(**gs.best_params_)
+            pipeline.set_params(**search.best_params_)
             print(step + ": ",
-                  gs.best_params_, ", time:", t,
+                  search.best_params_, ", time:", t,
                   "==========")
 
         md_dict[i] = pipeline
@@ -188,7 +237,7 @@ def customize_classifier(X, y, model_name, model_params='',
     # val score
     if val_score:
         val_info = cross_validate(best_est, X, y,
-                                  scoring=scoring,
+                                  scoring=scoring_dict,
                                   cv=5,
                                   return_train_score=True)
         val_info = pd.DataFrame(val_info)
@@ -198,13 +247,14 @@ def customize_classifier(X, y, model_name, model_params='',
     # fit best model
     best_est.fit(X, y)
 
+    # score
+    sc = scoring_dict[score]
+
     # add all
     res = pd.concat(
         [cv_result,
          pd.DataFrame({'params': [best_est.get_params()],
-                       'mean_test_score': [
-                           roc_auc_score(
-                               y, best_est.predict(X))],
+                       'mean_test_score': [sc(best_est['model'], X, y)],
                        'rank_test_score': ['all'],
                        'model': [best_md_name]})])
 
@@ -214,7 +264,7 @@ def customize_classifier(X, y, model_name, model_params='',
 if __name__ == '__main__':
     # sample
     X, y = make_classification(
-        n_samples=10000,
+        n_samples=1000,
         n_features=2,
         n_redundant=0,
         n_clusters_per_class=1,
@@ -227,9 +277,11 @@ if __name__ == '__main__':
     best_m, report, val_tb = customize_classifier(
         X=X,
         y=y,
-        model_name=['tree', 'rf', 'gbm',
-                    'xgb', 'lgb', 'lg'],
+        model_name=['lg'],
         model_params='',
+        search_method='bayes',
+        score='Balanced_Accuracy',
         imbalance=True,
+        imbalance_process={'under': {'sampling_strategy': 1}},
         val_score=True
     )
--- train.py
+++ train.py
@@ -11,25 +11,72 @@ import numpy as np
 import joblib
 import os
 
-
 train_params = {
-    'train_data_folder': 'E:/NPC/Train/',
-    'date_group': ['D105', 'D135', 'D165', 'D195', 'D375'],
-    'feature_file': 'C:/Users/admin/Desktop/ML_Paper/params/npc/fea_comb_file_2.csv',
-    'fea_group': {
-        'fea_comb1': ['all'],
-        'fea_comb2': ['all'],
-        'fea_comb3': ['all'],
-        'fea_comb4': ['all'],
-        'fea_comb5': ['all'],
-        'fea_comb6': ['all']
-    },
-    'target': 'NPC_D',
-    'model': ['lg', 'tree', 'rf', 'xgb', 'lgb'],
-    'output_folder': 'C:/Users/admin/Desktop/ML_Paper/result/NPC_20220811/',
-    'fea_plot_score': 'AUC'
-}
+    # process [training, plot]
+    "process": [1, 1],
+    # data params
+    "train_data_folder": "E:/NPC/Train/",
+    "data_group": ["D105", "D135", "D165", "D195", "D375"],
 
+    # feature selection params
+    "feature_file": "C:/Users/admin/Desktop/ML_Paper/params/npc/fea_comb_file_2.csv",
+    "fea_group": {
+        "fea_comb1": ["all"],
+        "fea_comb2": ["all"],
+        "fea_comb3": ["all"],
+        "fea_comb4": ["all"],
+        "fea_comb5": ["all"],
+        "fea_comb6": ["all"]
+    },
+
+    # model params
+    "target": "NPC_D",
+    "model": ["lg", "tree", "rf", "xgb", "lgb"],
+    "model_params": {
+        "model_params": {
+            "tree": {
+                "step1": {"model__max_depth": [3, 5]}
+            },
+            "rf": {
+                "step1": {"model__max_depth": [3, 5]}
+            },
+            "gbm": {
+                "step1": {"model__n_estimators": [10, 30, 50]},
+                "step2": {"model__max_depth": [3, 5]}
+            },
+            "xgb": {
+                "step1": {"model__max_depth": [3, 5],
+                          "model__min_child_weight": [1e-1, 1, 1e1]},
+                "step2": {"model__gamma": [0, 0.2]},
+                "step3": {"model__subsample": [0.8, 0.9]},
+                "step4": {"model__reg_alpha": [0, 1e-1, 1, 2]},
+                "step5": {"model__learning_rate": [0.1, 1]}
+            },
+            "lgb": {
+                "step1": {"model__num_leaves": [5, 10, 20]},
+                "step2": {"model__min_child_weight": [1e-1, 1, 1e1]},
+                "step3": {"model__subsample": [0.5, 0.8, 0.9, 1],
+                          "model__colsample_bytree": [0.5, 0.8, 0.9, 1]},
+                "step4": {"model__reg_alpha": [0, 1e-1, 1]}
+            },
+            "lg": {
+                "step1": {"model__penalty": ["l2"]}
+            }
+        },
+        "search_method": 'grid',
+        "score": "Balanced_Accuracy",
+        "imbalance": 1,
+        "imbalance_process": {'under': {'sampling_strategy': 1}},
+        "val_score": 1
+    },
+
+    # output params
+    "output_folder": "C:/Users/admin/Desktop/ML_Paper/result/NPC_20220811/",
+
+    # plot params
+    "plot_file": "",
+    "fea_plot_score": "AUC"
+}
 
 # step1. get train folder data
 file_v = os.listdir(train_params['train_data_folder'])
@@ -42,13 +89,11 @@ for i in ['/model', '/fea', '/plot']:
     f = train_params['output_folder'] + i
     Path(f).mkdir(parents=True, exist_ok=True)
 
-
 # step4. run
-df_report = pd.DataFrame()
-df_val_tb = pd.DataFrame()
 train_folder = train_params['train_data_folder']
-for d_group in train_params['date_group']:
-    print('date group: ', d_group)
+df_report, df_val_tb = pd.DataFrame(), pd.DataFrame()
+for d_group in train_params['data_group']:
+    print('data group: ', d_group)
     # get date group df
     train_path = [x for x in file_v if d_group in x]
     if len(train_path) > 1:
@@ -61,6 +106,7 @@ for d_group in train_params['date_group']:
         fea_all = fea_file[fea_file['Test_name'] == f_name]['Col'].to_list()
         for fea_method in fea_method_v:
             print('        fea method: ', fea_method)
+            # run fea method
             fea_now = f_s.fea_select(X=train_file[fea_all],
                                      y=train_file[train_params['target']],
                                      method=fea_method)
@@ -72,14 +118,13 @@ for d_group in train_params['date_group']:
 
             for md in train_params['model']:
                 print('            model: ', md)
-                best_m, report, val_tb = clf.customize_classifier(
-                    X=train_file[fea_now],
-                    y=train_file[train_params['target']],
-                    model_name=[md],
-                    model_params='',
-                    imbalance=True,
-                    val_score=True
-                )
+                md_params = train_params['model_params'].copy()
+                md_params['X'] = train_file[fea_now]
+                md_params['y'] = train_file[train_params['target']]
+                md_params['model_name'] = [md]
+
+                best_m, report, val_tb = \
+                    clf.customize_classifier(**md_params)
 
                 # save model
                 md_out_path = train_params['output_folder'] + '/model/' + d_group + '__' + f_name + '__' + \
@@ -87,18 +132,12 @@ for d_group in train_params['date_group']:
                 joblib.dump(best_m, md_out_path)
 
                 # save report
-                report['date_group'] = d_group
-                report['fea_group'] = f_name
-                report['fea_method'] = fea_method
-                report['model'] = md
+                report[['data_group', 'fea_group', 'fea_method', 'model']] = d_group,  f_name, fea_method, md
 
                 # save val
                 val_tb.index = val_tb.index.set_names(['val_time'])
-                val_tb['date_group'] = d_group
-                val_tb['fea_group'] = f_name
-                val_tb['fea_method'] = fea_method
-                val_tb['model'] = md
                 val_tb = val_tb.reset_index()
+                val_tb[['data_group', 'fea_group', 'fea_method', 'model']] = d_group,  f_name, fea_method, md
 
                 # update
                 df_report = pd.concat([df_report, report])
@@ -108,20 +147,23 @@ df_report.to_csv(train_params['output_folder'] + '/report.csv', index=False)
 df_val_tb.to_csv(train_params['output_folder'] + '/val_tb.csv', index=False)
 
 # step5. evaluation
-id_col = ['date_group', 'fea_group', 'fea_method', 'model']
+id_col = ['data_group', 'fea_group', 'fea_method', 'model']
 s = ['Accuracy', 'sensitivity', 'specificity', 'specificity',
      'Balanced_Accuracy', 'AUC', 'Kappa', 'F1']
 df_val = pd.read_csv(train_params['output_folder'] + '/val_tb.csv')
 df_m = pd.melt(df_val, id_vars=id_col, value_vars=['test_' + x for x in s] + ['train_' + x for x in s])
 df_m[['train_test', 's']] = df_m['variable'].str.split('_', 1, expand=True)
-df_ev = df_m.groupby(['date_group', 'fea_group', 'fea_method',
+df_ev = df_m.groupby(['data_group', 'fea_group', 'fea_method',
                       'model', 'train_test', 's'])['value'].mean().reset_index()
 df_ev.to_csv(train_params['output_folder'] + '/evaluation.csv', index=False)
 
 # step6. plot heatmap
-for d_group in train_params['date_group']:
+if train_params['plot_file'] != '':
+    df_ev = pd.read_csv(train_params['plot_file'])
+
+for d_group in train_params['data_group']:
     for t in ['train', 'test']:
-        df_plot = df_ev[(df_ev['date_group'] == d_group) &
+        df_plot = df_ev[(df_ev['data_group'] == d_group) &
                         (df_ev['s'] == train_params['fea_plot_score']) &
                         (df_ev['train_test'] == t)].copy()
         df_plot['fea'] = df_plot['fea_group'] + '__' + df_plot['fea_method']
@@ -129,6 +171,7 @@ for d_group in train_params['date_group']:
 
         df_plot = df_plot.pivot(index='model', columns='fea', values='value')
         harvest = df_plot.to_numpy()
+
         # plot
         fig, ax = plt.subplots()
         im = ax.imshow(harvest, cmap='YlGnBu')
@@ -155,5 +198,4 @@ for d_group in train_params['date_group']:
         fig.tight_layout()
         plt.show()
         plt.savefig(train_params['output_folder'] + '/plot/fea_select__' + d_group + '__' + t + '.png')
-        print('date_group: ', d_group, '_', t, 'done')
-
+        print('data_group: ', d_group, '_', t, 'done')
